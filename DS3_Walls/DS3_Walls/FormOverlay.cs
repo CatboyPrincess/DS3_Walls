d6cb3993912ac2c068aac0401e02e5e5cb36333d
hydra-chan <public.hydra.chan@gmail.com>
2018-02-06 19:14:03 -0800

    Timer to Re-paint and Process Memory Reading
    Need a way to scan for the correct address on every launch now

--- DS3_Walls/DS3_Walls/FormOverlay.cs
+++ DS3_Walls/DS3_Walls/FormOverlay.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -13,11 +14,22 @@ namespace DS3_Walls {
     public partial class FormOverlay : Form {
 
         private const string WINDOW_NAME = "DARK SOULS III";
+        const int PROCESS_WM_READ = 0x0010;
+        int timerInterval = (int)(1000 / 1); // 1000 / N where N is number of times per second
 
         IntPtr handle = FindWindow(null, WINDOW_NAME);
         RECT rect;
 
-        Graphics graphics;
+        static Graphics graphics;
+        static SolidBrush brush = new SolidBrush(Color.Red);
+        static Pen pen = new Pen(brush, 2.0f);
+        static Font font = new Font("Consolas", 12);
+
+        private Color backgroundColour = Color.Magenta;
+
+        private struct Player_Coordinates {
+            public float a, x, y, z; // the "a" stands for angle
+        }
 
         private struct RECT {
             public int left, top, right, bottom;
@@ -36,13 +48,19 @@ namespace DS3_Walls {
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
 
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
         public FormOverlay() {
             InitializeComponent();
         }
 
         private void FormOverlay_Load(object sender, EventArgs e) {
-            this.BackColor = Color.Magenta;
-            this.TransparencyKey = Color.Magenta;
+            this.BackColor = backgroundColour;
+            this.TransparencyKey = backgroundColour;
             this.TopMost = true;
             this.FormBorderStyle = FormBorderStyle.None;
             this.DoubleBuffered = true;
@@ -52,6 +70,7 @@ namespace DS3_Walls {
 
             bool result = GetWindowRect(handle, out rect);
             if (result) {
+                // size
                 this.Size = new Size(rect.right - rect.left, rect.bottom - rect.top);
                 this.Top = rect.top;
                 this.Left = rect.left;
@@ -61,19 +80,38 @@ namespace DS3_Walls {
                 this.Top = 0;
                 this.Left = 0;
             }
+
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer(); // please explicitly use System.Windows.Forms.Timer
+            timer.Interval = (timerInterval);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e) {
+            this.Invalidate();
         }
 
         private void FormOverlay_Paint(object sender, PaintEventArgs e) {
             graphics = e.Graphics;
-            SolidBrush brush = new SolidBrush(Color.White);
-            Pen pen = new Pen(brush, 2.0f);
-            Font font = new Font("Arial", 14);
 
-            int x = this.Left + 200;
-            int y = this.Top + 200;
+            int x = this.Right - 192;
+            int y = this.Top + 10;
 
-            graphics.DrawString("testing this thingy", font, brush, x, y);
-            graphics.DrawRectangle(pen, x, y, x + 100, y + 150);
+            // process
+            Process process = Process.GetProcessesByName("DarkSoulsIII")[0];
+            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+            int bytesRead = 0;
+            byte[] buffer = new byte[4]; // memory needed
+            Int64 address = 0x7FF5AEE12780; // placeholder. Still need a way to get the right address every time
+
+            ReadProcessMemory((int)processHandle, address, buffer, buffer.Length, ref bytesRead);
+
+            float coord_x = BitConverter.ToSingle(buffer, 0);
+            //System.Console.WriteLine(x);
+
+            // paint
+            //graphics.Clear(backgroundColour);
+            graphics.DrawString("Player Coordinates:\nA = na" + "\nX = " + coord_x + "\nY = na" + "\nZ = na", font, brush, x, y);
         }
     }
 }
